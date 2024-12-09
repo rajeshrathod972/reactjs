ff9cdc16422747727b99410793101ad516a18bb5
Rajesh Rathod <96156957+rajeshrathod972@users.noreply.github.com>
2024-12-09 21:01:00 +0530

    Update vite.config.js

--- cryptobank/vite.config.js
+++ cryptobank/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import svgr from '@svgr/plugin-vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),svgr()],
+  plugins: [react()],
 })
